7a2b6690f9170beebd0a8291a139219b2f149483
PaOneX <opcmicrosoft@gmail.com>
2025-12-03 23:03:13 +0530

    design navbar

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
+// async function loginpage(){
+//    let res = await fetch();
+//    let data = await res.text();
+//        document.getElementById("root").innerHTML=data;
+ 
+// }
+
 fetch("components/login/login.html").then(res=>res.text()).then(data=>{
     document.getElementById("root").innerHTML=data;
-})
+})  
 
-async function loginpage(){
-   let res = await fetch();
-   let data = await res.text();
-       document.getElementById("root").innerHTML=data;
- 
-}
 
-navigator.geolocation.getCurrentPosition(getPosition)
+// navigator.geolocation.getCurrentPosition(getPosition)
 
-function getPosition(position){
-console.log(position)
-}
+// function getPosition(position){
+// console.log(position)
+// }
