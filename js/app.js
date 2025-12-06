78c68de37cc4f43c2e4d3a172d31f0345682485a
PaOneX <opcmicrosoft@gmail.com>
2025-12-06 22:56:04 +0530

    create json file to load new arrivals

--- js/app.js
+++ js/app.js
@@ -9,13 +9,17 @@
 //     document.getElementById("root").innerHTML=data;
 // })  
 
- fetch("components/home/home.html").then(res=>res.text()).then(data=>{
-    document.getElementById("root").innerHTML=data;
-})  
-
-
 // navigator.geolocation.getCurrentPosition(getPosition)
 
 // function getPosition(position){
 // console.log(position)
 // }
+ fetch("components/home/home.html").then(res=>res.text()).then(data=>{
+    document.getElementById("root").innerHTML=data;
+}) 
+
+fetch("json/newArrivals.json")
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+    });
