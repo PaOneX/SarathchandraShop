9a967ab3e7a925f0e8b8dfe3c81b7183bf2ad416
PaOneX <opcmicrosoft@gmail.com>
2025-12-18 23:40:44 +0530

    store customer info in localStorage

--- js/register.js
+++ js/register.js
@@ -1,3 +1,25 @@
+let customerList =[];
+
 function register(){
     alert("Success");
+
+    let fname = document.getElementById("firstName").value
+    let lname = document.getElementById("lastName").value
+    let email = document.getElementById("email").value
+    let phone = document.getElementById("phone").value
+    let password = document.getElementById("password").value
+
+    let customer = {
+        fname : fname,
+        lname : lname,
+        email : email,
+        phone : phone,
+        password : password,
+    };
+
+    customerList.push(customer);
+    console.log(customerList);
+
+    localStorage.setItem("customer",JSON.stringify(customerList))
+    
 }
